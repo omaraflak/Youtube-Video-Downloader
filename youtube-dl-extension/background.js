5f5a5b06e7c8270fb4d2b4beca01b69b083a7422
Omar Aflak <aflakomar@gmail.com>
2020-08-18 21:59:40 +0200

    format selection with swal2

--- youtube-dl-extension/background.js
+++ youtube-dl-extension/background.js
@@ -1,9 +1,9 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     fetch(request.url)
         .then(response => response.json())
-        .then(data => {
-            chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-                chrome.tabs.sendMessage(tabs[0].id, data)
+        .then(response => {
+            chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+                chrome.tabs.sendMessage(tabs[0].id, {...request, response})
             })
         })
         .catch(error => {
--- youtube-dl-extension/contentScript.js
+++ youtube-dl-extension/contentScript.js
@@ -1,16 +1,50 @@
 const SERVER = 'http://localhost:8080'
 
-const onDownloadClicked = () => {
+let videoFormats = []
+
+const getVideoId = () => {
     let videoId = window.location.search.split('v=')[1]
     let ampersandPosition = videoId.indexOf('&')
-    if(ampersandPosition != -1) {
+    if (ampersandPosition != -1) {
         videoId = videoId.substring(0, ampersandPosition)
     }
-
-    chrome.runtime.sendMessage({url: `${SERVER}/start/?id=${videoId}`})
+    return videoId
 }
 
-const injectCode = (code) => {
+const fetchVideoFormats = () => {
+    const videoId = getVideoId()
+    chrome.runtime.sendMessage({uri: 'info', url: `${SERVER}/info/?id=${videoId}`})
+}
+
+const onFormatClicked = event => {
+    const formatId = event.target.id
+    const videoId = getVideoId()
+    chrome.runtime.sendMessage({uri: 'start', url: `${SERVER}/start/?id=${videoId}&format=${formatId}`})
+}
+
+const showFormatSelectionPopup = () => {
+    const buttonsHtml = videoFormats.map(format => {
+        return `<button id='${format.id}' class='format-button'>${format.format}</button>`
+    }).join('</br>')
+
+    Swal.fire({
+        title: 'Select a format',
+        icon: 'question',
+        html: buttonsHtml,
+        showCloseButton: true,
+        showConfirmButton: false,
+        onOpen: () => {
+            videoFormats.forEach(format => {
+                document.getElementById(format.id).onclick = event => {
+                    swal.close()
+                    onFormatClicked(event)
+                }
+            })
+        }
+    })
+}
+
+const injectCode = code => {
     const contents = document.querySelector("#meta-contents")
     if (contents == null) {
         return false
@@ -22,18 +56,11 @@ const injectCode = (code) => {
     topRow.appendChild(code)
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    const taskId = request.task_id
-    if (taskId) {
-        window.open(`${SERVER}/view/?task_id=${taskId}`, '_blank')
-    }
-})
-
-window.onload = () => {
+const injectButton = () => {
     const downloadButton = document.createElement('button')
     downloadButton.classList.add('download-button')
     downloadButton.innerHTML = 'DOWNLOAD'
-    downloadButton.onclick = onDownloadClicked
+    downloadButton.onclick = showFormatSelectionPopup
 
     const injectLoop = () => {
         if (!injectCode(downloadButton)) {
@@ -42,4 +69,18 @@ window.onload = () => {
     }
 
     injectLoop()
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.uri == 'start') {
+        const taskId = request.response.task_id
+        window.open(`${SERVER}/view/?task_id=${taskId}`, '_blank')
+    } else if (request.uri == 'info') {
+        videoFormats = request.response
+        injectButton()
+    }
+})
+
+window.onload = () => {
+    fetchVideoFormats()
 }
